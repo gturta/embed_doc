7175eee10069965c5635e071dce3c06bd49295ce
Gabriel Turta <gturta@gmail.com>
2026-06-30 15:08:56 +0300

    parse from azure section spans

--- src/document_intelligence.rs
+++ src/document_intelligence.rs
@@ -33,19 +33,27 @@ pub struct AnalyzeResult{
     #[serde(rename="stringIndexType")]
     string_index_type: String,
     pub content: String,
+    #[serde(skip)]
+    utf16_content: Vec<u16>,
     figures: Vec<DocumentFigure>,
     pages: Vec<DocumentPage>,
     paragraphs: Vec<DocumentParagraph>,
     tables: Vec<DocumentTable>,
+    sections: Vec<DocumentSection>,
 }
 #[derive(Deserialize, Serialize)]
+pub struct DocumentSection{
+    elements: Vec<String>,
+    spans: Vec<Span>,
+}
+#[derive(Deserialize, Serialize, Clone)]
 pub struct DocumentFigure{
     id: Option<String>,
     caption: Option<DocumentCaption>,
     elements: Option<Vec<String>>,
     spans: Vec<Span>,
 }
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct DocumentCaption{
     content: String,
     elements: Vec<String>,
@@ -57,7 +65,7 @@ pub struct DocumentPage{
     page_number: u16,
     spans: Vec<Span>,
 }
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 pub struct DocumentTable{
     #[serde(rename="rowCount")]
     row_count: u16,
@@ -65,16 +73,16 @@ pub struct DocumentTable{
     column_count: u16,
     spans: Vec<Span>,
 }
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize,Clone)]
 pub struct DocumentParagraph{
     content: String,
     role: Option<String>,
     spans: Vec<Span>,
 }
-#[derive(Deserialize, Serialize)]
+#[derive(Deserialize, Serialize, Clone)]
 struct Span{
-    offset: u32,
-    length: u32,
+    offset: usize,
+    length: usize,
 }
 
 pub async fn send_file_to_analyze(input: String, azure: &AzureConfig) -> Result<DIAnalyzeHandle, AppError> {
@@ -152,4 +160,120 @@ pub async fn get_analyze_result(handle: DIAnalyzeHandle, azure: &AzureConfig) ->
         };
     }
     Err(AppError::Azure("could not get analyze results".to_string()))
+}
+
+
+pub enum DocElement{
+    Node(DocNode),
+    Paragraph(String),
+    Table(String),
+    Figure(String),
+}
+pub struct DocNode{
+    pub children: Vec<DocElement>,
+}
+
+impl std::fmt::Display for DocNode{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        for child in &self.children {
+            write!(f, "{}", child)?;
+        }
+        Ok(())
+    }
+}
+impl std::fmt::Display for DocElement{
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self{
+            DocElement::Paragraph(paragraph) => {
+                write!(f, "<PARAGRAPH>\n{}\n</PARAGRAPH>\n", paragraph)
+            },
+            DocElement::Table(table) => {
+                writeln!(f, "<TABLE>\n{}\n</TABLE>\n", table)
+            },
+            DocElement::Figure(figure) => {
+                write!(f, "<FIGURE>\n{}\n</FIGURE>\n", figure)
+            },
+            DocElement::Node(node) => write!(f, "{}", node),
+        }
+    }
+}
+
+pub fn tree_from_analyze_result(input: &mut AnalyzeResult) -> Result<DocNode, AppError> {
+    //convert the content to a utf16 vec to be able to slice it by spans
+    input.utf16_content = input.content.encode_utf16().collect();
+    let mut root = DocNode{ children: Vec::new() };
+    //get first section as the root of the tree
+    let root_section = input.sections.first()
+        .ok_or(AppError::Azure("No root section in AnalyzeResult".to_string()))?;
+    for elem_str in &root_section.elements {
+        let element = element_from_str(elem_str, input)?;
+        root.children.push(element);
+    }
+    Ok(root)
+}
+
+fn get_span_text(span: &Span, analyze: &AnalyzeResult) -> String {
+    let content_slice = &analyze.utf16_content[span.offset..span.offset + span.length];
+    String::from_utf16(content_slice).unwrap_or("<!!! Span not found !!!>".to_string())
+}
+
+fn element_from_str(elem_str: &str, input: &AnalyzeResult) -> Result<DocElement, AppError> {
+    //elem_str should be: "/paragraphs/5" or "/tables/4" or "/figures/1" or "/sections/5"
+    let splitted: Vec<&str> = elem_str.split('/').collect();
+    if splitted.len() != 3 {
+        return Err(AppError::Azure(format!("Invalid element selector {}, got {:?}", elem_str, splitted)));
+    }
+    let elem_type = splitted[1];
+    let index: usize = splitted[2].parse().expect("Invalid element index");
+    let element = match elem_type {
+        "paragraphs" => {
+            let Some(paragraph) = input.paragraphs.get(index) else {
+                return Err(AppError::Azure(format!("Paragraph with id {} not found", index)));
+            };
+            let mut content = String::new();
+            for span in &paragraph.spans {
+                content.push_str(&get_span_text(span, input));
+            }
+            DocElement::Paragraph(content)
+        },
+        "tables" => {
+            let Some(table) = input.tables.get(index) else {
+                return Err(AppError::Azure(format!("Table with id {} not found", index)));
+            };
+            let mut content = String::new();
+            for span in &table.spans {
+                content.push_str(&get_span_text(span, input));
+            }
+            DocElement::Table(content)
+        },
+        "figures" => {
+            let Some(figure) = input.figures.get(index) else {
+                return Err(AppError::Azure(format!("Figure with id {} not found", index)));
+            };
+            let mut content = String::new();
+            for span in &figure.spans {
+                content.push_str(&get_span_text(span, input));
+            }
+            DocElement::Figure(content)
+        },
+        "sections" => {
+            let Some(section) = input.sections.get(index) else {
+                return Err(AppError::Azure(format!("Section with id {} not found", index)));
+            };
+            DocElement::Node(node_from_section(section, input))
+        },
+        _ => return Err(AppError::Azure(format!("Invalid element selector {}", elem_str))),
+
+    };
+    Ok(element)
+}
+
+fn node_from_section(section: &DocumentSection, analysis: &AnalyzeResult) -> DocNode {
+    let mut node = DocNode{ children: Vec::new() };
+    for element_str in &section.elements {
+        if let Ok(element) = element_from_str(element_str, analysis) {
+            node.children.push(element);
+        }
+    }
+    node
 }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use reqwest::Client;
 use clap::{Parser,Subcommand};
 use std::io::{BufWriter, Write};
-use markdown::{to_mdast, ParseOptions, mdast::Node};
 
 mod error;
 use error::AppError;
@@ -112,123 +111,12 @@ fn process(input: String, output: String) {
     let input_str = std::fs::read_to_string(&input).expect("Input file should be readable");
     let op: AnalyzeOperation = serde_json::from_str(&input_str).expect("Input file should contain a AnalyzeOperation instance");
     //write markdown
-    if let Some(result) = op.analyze_result {
-        let tree = to_mdast(&result.content, &ParseOptions::default())
-            .expect("unable to parse markdown contents into AST");
+    if let Some(mut result) = op.analyze_result {
         let file = std::fs::File::create(output).expect("Ouput file shold be writable");
         let mut writer = BufWriter::new(file);
-        //debug_print_tree(&tree, 0);
-        traverse_ast(&tree, 0, &mut writer);
+        let tree = document_intelligence::tree_from_analyze_result(&mut result).expect("could not parse doc tree");
+        write!(writer, "{}", tree).expect("could not write tree to file");
     }
 }
 
-fn traverse_ast(node: &Node, level: usize, writer: &mut impl Write) {
-    //process node
-    match node {
-        Node::Heading(_) => {
-            //track headers
-            eprintln!("Node::Heading({level}) {}", extract_text(node));
-        },
-        Node::Paragraph(_) => {
-            let text = extract_text(node);
-            writeln!(writer, "{}", text).unwrap();
-        },
-        Node::Html(_) => {
-            let text = extract_text(node);
-            writeln!(writer, "{}", text).unwrap();
-        },
-        Node::Table(table) => {
-            eprintln!("Node::Table");
-            let text = extract_table(table);
-            writeln!(writer, "{}", text).unwrap();
-        },
-        _ => {
-            //process children
-            if let Some(children) = node.children() {
-                for child in children {
-                    traverse_ast(child, level+1, writer);
-                }
-            }
-        },
-    };
-}
 
-fn extract_table(table: &markdown::mdast::Table) -> String {
-    let mut output = String::new();
-    for row in &table.children {
-        output.push_str("| ");
-        if let Some(cells) = row.children() {
-            let text: Vec<String> = cells.iter()
-                .map(extract_text)
-                .collect();
-            output.push_str(&text.join(" | "));
-        }
-        output.push_str(" |");
-    }
-    eprintln!("TABLE: {}",output);
-    output
-}
-
-fn extract_text(node: &Node) -> String {
-    match node {
-        Node::Text(text) => text.value.clone(),
-        Node::InlineCode(code) => code.value.clone(),
-        Node::Html(html) => html.value.clone(),
-        _ => {
-            let mut result = String::new();
-            if let Some(children) = node.children() {
-                for child in children {
-                    result.push_str(&extract_text(child));
-                }
-            }
-            result
-        }
-    }
-}
-
-fn debug_print_tree(node: &Node, depth: usize) {
-    eprintln!("{}{}", " ".repeat(depth), node_name(node));
-    if let Some(children) = node.children() {
-        for child in children {
-            debug_print_tree(child, depth+1);
-        }
-    }
-}
-fn node_name(node: &Node) -> &'static str {
-    match node {
-        Node::Root(_) => "Root",
-        Node::Blockquote(_) => "Blockquote",
-        Node::FootnoteDefinition(_) => "FootnoteDefinition",
-        Node::MdxJsxFlowElement(_) => "MdxJsxFlowElement",
-        Node::List(_) => "List",
-        Node::MdxjsEsm(_) => "MdxjsEsm",
-        Node::Toml(_) => "Toml",
-        Node::Yaml(_) => "Yaml",
-        Node::Break(_) => "Break",
-        Node::InlineCode(_) => "InlineCode",
-        Node::InlineMath(_) => "InlineMath",
-        Node::Delete(_) => "Delete",
-        Node::Emphasis(_) => "Emphasis",
-        Node::MdxTextExpression(_) => "MdxTextExpression",
-        Node::FootnoteReference(_) => "FootnoteReference",
-        Node::Html(_) => "Html",
-        Node::Image(_) => "Image",
-        Node::ImageReference(_) => "ImageReference",
-        Node::MdxJsxTextElement(_) => "MdxJsxTextElement",
-        Node::Link(_) => "Link",
-        Node::LinkReference(_) => "LinkReference",
-        Node::Strong(_) => "Strong",
-        Node::Text(_) => "Text",
-        Node::Code(_) => "Code",
-        Node::Math(_) => "Math",
-        Node::MdxFlowExpression(_) => "MdxFlowExpression",
-        Node::Heading(_) => "Heading",
-        Node::Table(_) => "Table",
-        Node::ThematicBreak(_) => "ThematicBreak",
-        Node::TableRow(_) => "TableRow",
-        Node::TableCell(_) => "TableCell",
-        Node::ListItem(_) => "ListItem",
-        Node::Definition(_) => "Definition",
-        Node::Paragraph(_) => "Paragraph",
-    }
-}
